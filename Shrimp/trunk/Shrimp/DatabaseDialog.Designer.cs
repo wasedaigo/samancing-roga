0106e67573f7e0f73e5bffff6248f8ab212308ed
hajimehoshi <hajimehoshi@5d42db64-0766-11de-b17c-0f1ef23b492c>
2009-03-09 21:39:00 +0000

    modified DatabaseDialog

--- Shrimp/trunk/Shrimp/DatabaseDialog.Designer.cs
+++ Shrimp/trunk/Shrimp/DatabaseDialog.Designer.cs
@@ -29,47 +29,44 @@
         private void InitializeComponent()
         {
             this.tabControl1 = new System.Windows.Forms.TabControl();
-            this.tabPage1 = new System.Windows.Forms.TabPage();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.TileSetTabPage = new System.Windows.Forms.TabPage();
             this.OKButton = new System.Windows.Forms.Button();
             this.cancelButton = new System.Windows.Forms.Button();
             this.ApplyButton = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.TileSetLabel = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.tabControl1.SuspendLayout();
+            this.TileSetTabPage.SuspendLayout();
             this.SuspendLayout();
             // 
             // tabControl1
             // 
-            this.tabControl1.Controls.Add(this.tabPage1);
-            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.TileSetTabPage);
             this.tabControl1.Location = new System.Drawing.Point(8, 8);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(272, 200);
+            this.tabControl1.Size = new System.Drawing.Size(800, 600);
             this.tabControl1.TabIndex = 0;
             // 
-            // tabPage1
+            // TileSetTabPage
             // 
-            this.tabPage1.Location = new System.Drawing.Point(4, 21);
-            this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(264, 175);
-            this.tabPage1.TabIndex = 0;
-            this.tabPage1.Text = "tabPage1";
-            this.tabPage1.UseVisualStyleBackColor = true;
-            // 
-            // tabPage2
-            // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 21);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(192, 75);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "tabPage2";
-            this.tabPage2.UseVisualStyleBackColor = true;
+            this.TileSetTabPage.Controls.Add(this.tableLayoutPanel1);
+            this.TileSetTabPage.Controls.Add(this.button1);
+            this.TileSetTabPage.Controls.Add(this.TileSetLabel);
+            this.TileSetTabPage.Controls.Add(this.listBox1);
+            this.TileSetTabPage.Location = new System.Drawing.Point(4, 21);
+            this.TileSetTabPage.Name = "TileSetTabPage";
+            this.TileSetTabPage.Padding = new System.Windows.Forms.Padding(3);
+            this.TileSetTabPage.Size = new System.Drawing.Size(792, 575);
+            this.TileSetTabPage.TabIndex = 0;
+            this.TileSetTabPage.Text = "Tile Set";
+            this.TileSetTabPage.UseVisualStyleBackColor = true;
             // 
             // OKButton
             // 
-            this.OKButton.Location = new System.Drawing.Point(24, 216);
+            this.OKButton.Location = new System.Drawing.Point(552, 616);
             this.OKButton.Name = "OKButton";
             this.OKButton.Size = new System.Drawing.Size(80, 24);
             this.OKButton.TabIndex = 1;
@@ -79,7 +76,7 @@
             // 
             // cancelButton
             // 
-            this.cancelButton.Location = new System.Drawing.Point(112, 216);
+            this.cancelButton.Location = new System.Drawing.Point(640, 616);
             this.cancelButton.Name = "cancelButton";
             this.cancelButton.Size = new System.Drawing.Size(80, 24);
             this.cancelButton.TabIndex = 2;
@@ -89,12 +86,62 @@
             // 
             // ApplyButton
             // 
-            this.ApplyButton.Location = new System.Drawing.Point(200, 216);
+            this.ApplyButton.Location = new System.Drawing.Point(728, 616);
             this.ApplyButton.Name = "ApplyButton";
             this.ApplyButton.Size = new System.Drawing.Size(80, 24);
             this.ApplyButton.TabIndex = 3;
             this.ApplyButton.Text = "&Apply";
             this.ApplyButton.UseVisualStyleBackColor = true;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.IntegralHeight = false;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(8, 48);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.ScrollAlwaysVisible = true;
+            this.listBox1.Size = new System.Drawing.Size(160, 488);
+            this.listBox1.TabIndex = 0;
+            // 
+            // TileSetLabel
+            // 
+            this.TileSetLabel.BackColor = System.Drawing.Color.Black;
+            this.TileSetLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.TileSetLabel.ForeColor = System.Drawing.Color.White;
+            this.TileSetLabel.Location = new System.Drawing.Point(8, 8);
+            this.TileSetLabel.Name = "TileSetLabel";
+            this.TileSetLabel.Size = new System.Drawing.Size(160, 32);
+            this.TileSetLabel.TabIndex = 1;
+            this.TileSetLabel.Text = "Tile Set";
+            this.TileSetLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.Location = new System.Drawing.Point(8, 544);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(160, 24);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Change Max...";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.tableLayoutPanel1.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Inset;
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(176, 8);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(608, 560);
+            this.tableLayoutPanel1.TabIndex = 4;
             // 
             // DatabaseDialog
             // 
@@ -102,7 +149,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
             this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
-            this.ClientSize = new System.Drawing.Size(611, 523);
+            this.ClientSize = new System.Drawing.Size(939, 767);
             this.Controls.Add(this.ApplyButton);
             this.Controls.Add(this.cancelButton);
             this.Controls.Add(this.OKButton);
@@ -115,6 +162,7 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Database";
             this.tabControl1.ResumeLayout(false);
+            this.TileSetTabPage.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -122,10 +170,13 @@
         #endregion
 
         private System.Windows.Forms.TabControl tabControl1;
-        private System.Windows.Forms.TabPage tabPage1;
-        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage TileSetTabPage;
         private System.Windows.Forms.Button OKButton;
         private System.Windows.Forms.Button cancelButton;
         private System.Windows.Forms.Button ApplyButton;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label TileSetLabel;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
     }
 }
--- Shrimp/trunk/Shrimp/Util.cs
+++ Shrimp/trunk/Shrimp/Util.cs
@@ -14,9 +14,9 @@ namespace Shrimp
             BackgroundBitmap = new Bitmap(32, 32);
             using (Graphics g = Graphics.FromImage(BackgroundBitmap))
             {
-                Color color1 = Color.FromArgb(0, 0, 0x66);
+                Color color1 = Color.FromArgb(0, 0, 0x80);
                 Brush brush1 = new SolidBrush(color1);
-                Color color2 = Color.FromArgb(0, 0, 0x33);
+                Color color2 = Color.FromArgb(0, 0, 0x40);
                 Brush brush2 = new SolidBrush(color2);
                 g.FillRectangle(brush1, 0, 0, 16, 16);
                 g.FillRectangle(brush2, 0, 16, 16, 16);
